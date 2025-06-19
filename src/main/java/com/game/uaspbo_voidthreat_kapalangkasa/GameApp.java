96958ff2a5ad1491cab98bdac78ffb7fca23031c
edwin <edwinjonatanpurba@gmail.com>
2025-06-19 19:45:27 +0700

    Button Hover

--- src/main/java/com/game/uaspbo_voidthreat_kapalangkasa/GameApp.java
+++ src/main/java/com/game/uaspbo_voidthreat_kapalangkasa/GameApp.java
@@ -3,6 +3,7 @@ package com.game.uaspbo_voidthreat_kapalangkasa;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,6 +11,9 @@ import java.io.IOException;
 public class GameApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        Font font = Font.loadFont(getClass().getResource("/com/game/uaspbo_voidthreat_kapalangkasa/assets/PressStart2P-Regular.ttf").toExternalForm(), 12);
+
         FXMLLoader fxmlLoader = new FXMLLoader(GameApp.class.getResource("MainMenu.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Void Threat");
